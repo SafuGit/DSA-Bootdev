e46afad436e1f007cdeb2415eb721fa00004b901
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-10-19 15:50:52 +0600

    feat(sort): implement quick sort algorithm
    - implement quick_sort function with partition
    - add partition function to divide array based on pivot

--- CH4-L15/main.py
+++ CH4-L15/main.py
@@ -1,6 +1,16 @@
 def quick_sort(nums, low, high):
-    pass
+  if low < high:
+    middle = partition(nums, low, high)
+    quick_sort(nums, low, middle - 1)
+    quick_sort(nums, middle + 1, high)
 
 
-def partition(nums, low, high):
-    pass
+def partition(nums: list, low, high):
+    pivot = nums[high]
+    i = low - 1
+    for j in range(low, high):
+      if nums[j] < pivot:
+        i += 1
+        nums[i], nums[j] = nums[j], nums[i]
+    nums[high], nums[i + 1] = nums[i + 1], nums[high]
+    return i + 1
