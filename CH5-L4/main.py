8cb0da1ee218f74a7cdc0ae3be8abfd3c88b8c16
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-10-19 18:55:20 +0600

    feat(algorithm): implement power set function
    - implement power_set function to return all subsets of input set
    - uses iterative approach to generate subsets

--- CH5-L4/main.py
+++ CH5-L4/main.py
@@ -1,2 +1,13 @@
 def power_set(input):
-    pass
+  if len(input) == 0:
+    return [[]]
+  else:
+    all_subsets = [[]]
+    for elem in input:
+      new_subsets = []
+      for subset in all_subsets:
+        subset_copy = subset
+        subset_copy = new_subsets + [elem]
+        new_subsets.append(subset_copy)
+      all_subsets.append(new_subsets)
+    return all_subsets
