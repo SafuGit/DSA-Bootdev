78c8bb770be5a5b99e45dedfc95e10fc9938808f
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-10-20 16:48:21 +0600

    feat(stack): implement balanced parentheses validation

--- CH7-L7/main.py
+++ CH7-L7/main.py
@@ -1,5 +1,14 @@
 from stack import Stack
 
-
 def is_balanced(input_str):
-  pass
+  stack = Stack()
+  for bracket in input_str:
+    if bracket == "(":
+      stack.push("(")
+    if bracket == ")":
+      if stack.size() == 0:
+        return False
+      stack.pop()
+  if stack.size() == 0:
+    return True
+  return False
