923813e480881a4ade0de89d49479c86a7d4ec2a
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-10-20 15:35:55 +0600

    feat(main): implement count_marketers function
    - implements the count_marketers function to count the number of "marketer" job titles in a list
    - iterates through the list of job titles
    - increments the count if a job title is "marketer" (case-insensitive)
    - returns the final count

--- CH6-L1/main.py
+++ CH6-L1/main.py
@@ -1,2 +1,6 @@
-def count_marketers(job_titles):
-    pass
+def count_marketers(job_titles: list[str]):
+  count = 0
+  for job in job_titles:
+    if job.lower() == "marketer":
+      count += 1
+  return count
